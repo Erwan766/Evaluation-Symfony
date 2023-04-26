67f882b2b10176e71b6f13f1f0d69995c14b058c
Erwan <erwanp-2001@hotmail.fr>
2023-04-26 21:50:01 +0200

    V2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use doctrine;
 use App\Entity\Produit;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    // le champ: '' est vide pour que ça soit la page par défaut
     #[Route('/', name: 'app_home')]
     public function index(ManagerRegistry $doctrine): Response
     {
---
+++ src/Entity/Produit.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column]
+    private ?int $stock = null;
+
+    #[ORM\OneToMany(mappedBy: 'produitId', targetEntity: Commentaire::class)]
+    private Collection $commentaireId;
+
+    public function __construct()
+    {
+        $this->commentaireId = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commentaire>
+     */
+    public function getCommentaireId(): Collection
+    {
+        return $this->commentaireId;
+    }
+
+    public function addCommentaireId(Commentaire $commentaireId): self
+    {
+        if (!$this->commentaireId->contains($commentaireId)) {
+            $this->commentaireId->add($commentaireId);
+            $commentaireId->setProduitId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommentaireId(Commentaire $commentaireId): self
+    {
+        if ($this->commentaireId->removeElement($commentaireId)) {
+            // set the owning side to null (unless already changed)
+            if ($commentaireId->getProduitId() === $this) {
+                $commentaireId->setProduitId(null);
+            }
+        }
+
+        return $this;
+    }
+}
